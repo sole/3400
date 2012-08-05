b79719559813d404ecffbf35688d7fe842245923
sole <listas@soledadpenades.com>
2012-08-05 19:54:34 +0200

    Add script for packing JS

---
+++ utils/build.py
@@ -0,0 +1,57 @@
+import os
+import urllib.request, urllib.parse, urllib.error
+
+files = [
+	'js/3400.js',
+	'js/3400_DisplayControls.js',
+	'js/3400_Objects.js',
+	'js/CollisionUtils.js',
+	'js/StringFormat.js',
+	'js/utils.js'
+]
+
+outfile = '../js/main_compressed.js'
+
+# ~~~
+
+def get_response(data):
+	post_data = urllib.parse.urlencode(data)
+	request = urllib.request.Request('http://closure-compiler.appspot.com/compile', post_data.encode())
+	response = urllib.request.urlopen( request )
+
+	compressed = response.read()
+	
+	return compressed
+
+string = ''
+
+for filename in files:
+	with open('../' + filename, 'r') as f:
+		string += f.read() + "\n"
+
+print('Files collected')
+
+print('Calling Closure API service...')
+
+values = {
+	'compilation_level': 'SIMPLE_OPTIMIZATIONS',
+	'output_format': 'text',
+	'output_info': 'compiled_code',
+	'js_code': string
+}
+
+compressed_js = get_response( values )
+
+print('compressed size', len( compressed_js) )
+
+if len(compressed_js) == 1:
+	print('BOOOH ERRORS FOUND!!!')
+	values['output_info'] = 'errors'
+	
+	errors = get_response( values )
+	print(errors)
+
+else:
+
+	with open(outfile, 'wb') as f:
+		f.write(compressed_js)
